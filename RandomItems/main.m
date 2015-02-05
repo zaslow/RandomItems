21cdd0df11144d2f279a11258af057522629ff21
Christine Gornall <cgornall@Christine-Gornalls-MacBook-Pro.local>
2015-02-04 21:53:54 -0500

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Christine Gornall on 2/4/15.
+//  Copyright (c) 2015 Dean Zaslow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
